7fb5611f4b8f18f582a30961696c80e587a7cfe0
David Gilbertson <gilbertson.david@gmail.com>
2018-10-12 21:06:33 +1100

    Better mock data

--- src/data/mock.js
+++ src/data/mock.js
@@ -1,78 +1,62 @@
 export default {
-  name: 'An example: a comparison of network types',
+  name: 'An example: 9% random variation',
   scenarios: [
     {
-      name: '3G, no cache',
-      data: [120, 315, 500, 541, 512, 511],
+      id: '8bae2979-6b9f-4e63-80e4-f0a5993c455e',
+      name: 'Random set 1',
+      data: [
+        1202,
+        4686,
+        134,
+        3220,
+        1014,
+        2494,
+        2700,
+        1253,
+        3580,
+        2655,
+        1849,
+        2893,
+        1755,
+        3202,
+        1242,
+        4298,
+        1678,
+        4478,
+        2424,
+        3895,
+        1866,
+        2494
+      ]
     },
     {
-      name: '4G, no cache',
-      data: [352, 305, 450, 341, 312, 211],
-    },
-    {
-      name: 'Broadband no cache',
-      data: [402, 399, 331, 301, 325]
-    },
-    // {
-    //   name: 'Android/Chrome (Note 9) HTTP 2 Cache 19 files',
-    //   data: [317, 300, 316, 300, 313]
-    // },
-    // {
-    //   name: 'Android/Chrome (Note 9) HTTP 2 Wifi 1 file',
-    //   data: [355, 462, 605, 683, 489]
-    // },
-    // {
-    //   name: 'Android/Chrome (Note 9) HTTP 2 Wifi 19 files',
-    //   data: [361, 352, 343, 353, 343]
-    // },
-    // {
-    //   name: 'Android/Chrome (Note 9) HTTP 2 4G 1 file',
-    //   data: [745, 646, 696, 1116, 997]
-    // },
-    // {
-    //   name: 'Android/Chrome (Note 9) HTTP 2 4G 19 files',
-    //   data: [428, 483, 425, 467, 473]
-    // },
-    // {
-    //   name: 'Android/Chrome (Note 9) HTTP 2 3G 1 file',
-    //   data: [921, 845, 891, 878, 710]
-    // },
-    // {
-    //   name: 'Android/Chrome (Note 9) HTTP 2 3G 19 files',
-    //   data: [733, 611, 676, 631, 597]
-    // },
-    // {
-    //   name: 'Windows 8/IE11 HTTP 1.1 Fibre 1 file',
-    //   data: [342, 312, 337, 358, 307]
-    // },
-    // {
-    //   name: 'Windows 8/IE11 HTTP 1.1 Fibre 19 files',
-    //   data: [346, 425, 356, 350, 385]
-    // },
-    // {
-    //   name: 'Android/Chrome (Pixel 1) HTTP 2 Cache 1 file',
-    //   data: [800, 198, 165, 239, 212, 197, 234, 157, 291, 221, 229, 287, 293]
-    // },
-    // {
-    //   name: 'Android/Chrome (Pixel 1) HTTP 2 Cache 19 files',
-    //   data: [259, 366, 276, 243, 259, 380, 331, 356, 378, 314, 284, 288]
-    // },
-    // {
-    //   name: 'Android/Chrome (Pixel 1) HTTP 2 Wifi 1 file',
-    //   data: [312, 321, 385, 389, 330, 503, 346, 461, 441, 458, 514, 341]
-    // },
-    // {
-    //   name: 'Android/Chrome (Pixel 1) HTTP 2 Wifi 19 files',
-    //   data: [500, 471, 423, 519, 409, 661, 625, 651, 724, 610, 752, 688]
-    // },
-    // {
-    //   name: 'Android/Chrome (Pixel 1) HTTP 2 4G 1 file',
-    //   data: [815, 842, 1056, 988, 688, 578, 560, 651, 592, 540, 625, 586]
-    // },
-    // {
-    //   name: 'Android/Chrome (Pixel 1) HTTP 2 4G 19 files',
-    //   data: [748, 676, 656, 629, 632, 1046, 956, 737, 1020, 1077, 944, 628]
-    // },
+      id: '1299ec10-4775-4825-abf6-f59ea1e026a9',
+      name: 'Random set 2',
+      data: [
+        793,
+        910,
+        3178,
+        4087,
+        3509,
+        2042,
+        3444,
+        2278,
+        1758,
+        3382,
+        2508,
+        1628,
+        1825,
+        139,
+        4832,
+        505,
+        4700,
+        4765,
+        4376,
+        846,
+        1473,
+        2278
+      ]
+    }
   ],
 };
 
